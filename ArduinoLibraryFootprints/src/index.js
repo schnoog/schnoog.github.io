0d3daf00ad368e767c4c00aef7047fdfac454080
Volker Braun <schnoog@schnoog.eu>
2023-05-02 19:59:27 +0200

    Updated

--- ArduinoLibraryFootprints/src/index.js
+++ ArduinoLibraryFootprints/src/index.js
@@ -1,3 +1,4 @@
+/*
 const grid = new gridjs.Grid({
   columns: ['Name', 'URL', 'Version', 'Min Program Space', 'Min Dynamic Space', 'Test-Platform' ],
   server: {
@@ -8,3 +9,57 @@ sort: true,
 search: true
 
 }).render(document.getElementById("wrapper"));
+*/
+
+
+
+const grid = new gridjs.Grid({
+  columns: [
+	{
+    	name: "Name",
+    	sort: true,
+	width: '25%'
+  	},
+	{
+    	name: "URL",
+    	sort: true,
+	width: '25%',
+//	formatter: (cell) => gridjs.html(`${cell}`)
+  	},
+	{
+    	name: "Version",
+    	sort: true,
+	width: '10%'
+  	},
+	{
+    	name: "Min.Prog.Space",
+    	sort: true,
+	width: '15%'
+  	},
+	{
+    	name: "Min.Dyn.Space",
+    	sort: true,
+	width: '15%'
+  	},
+	{
+    	name: "Test-platform",
+    	sort: true,
+	width: '10%'
+  	}
+
+
+
+
+
+ ],
+  server: {
+    url: 'https://raw.githubusercontent.com/schnoog/ArduinoLibraryFootprintAnalyzer/main/php/dump.json?' + Date.now(),
+    then: data => data.data.map(lib => [lib.lib_name, gridjs.html("<a href='" + lib.lib_url + "' target='_blank' title='" + lib.lib_url + "'><small>" +  lib.lib_url + "</small></a>"), lib.lib_version, lib.lib_minprogspace, lib.lib_mindynspace, lib.lib_platform])
+  },
+sort: true,
+search: true
+
+}).render(document.getElementById("wrapper"));
+
+
+
