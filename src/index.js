79577fe4cc6dd383f036d2d4621c73e4eea7b2f3
Volker Braun <schnoog@schnoog.eu>
2023-05-02 16:57:55 +0200

    add

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const grid = new gridjs.Grid({
-  columns: ['Name', 'URL', 'Version', 'Min Program Space', 'Min Dynamic Space'],
+  columns: ['Name', 'URL', 'Version', 'Min Program Space', 'Min Dynamic Space', 'Test-Platform' ],
   server: {
     url: 'https://raw.githubusercontent.com/schnoog/ArduinoLibraryFootprintAnalyzer/main/php/dump.json?' + Date.now(),
-    then: data => data.data.map(lib => [lib.lib_name, lib.lib_url, lib.lib_version, lib.lib_minprogspace, lib.lib_mindynspace])
+    then: data => data.data.map(lib => [lib.lib_name, lib.lib_url, lib.lib_version, lib.lib_minprogspace, lib.lib_mindynspace, lib.lib_platform])
   },
 sort: true,
 search: true
